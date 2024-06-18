1c1eb78a14a23c9654b149e6c57d64037cf552d4
horakdave <horakdejv@gmail.com>
2024-06-18 10:16:59 +0200

    Update raycasting-3D-map.py

--- raycasting-3D-map/raycasting-3D-map.py
+++ raycasting-3D-map/raycasting-3D-map.py
@@ -5,27 +5,26 @@ from OpenGL.GLU import *
 import math
 import numpy as np
 
-def create_cube():  # Vertecies and edges of a cube
+def load_obj(filename):
+    vertices = []
+    faces = []
+    with open(filename, 'r') as file:
+        for line in file:
+            if line.startswith('v '):
+                vertices.append(tuple(map(float, line.strip().split()[1:])))
+            elif line.startswith('f'):
+                face = [int(i.split('/')[0]) - 1 for i in line.strip().split()[1:]]
+                faces.append(face)
+    return vertices, faces
 
-    vertices = (
-        (1, -1, -1), (1, 1, -1), (-1, 1, -1), (-1, -1, -1),
-        (1, -1, 1), (1, 1, 1), (-1, -1, 1), (-1, 1, 1)
-    )
-    edges = (
-        (0, 1), (1, 2), (2, 3), (3, 0),
-        (4, 5), (5, 6), (6, 7), (7, 4),
-        (0, 4), (1, 5), (2, 6), (3, 7)
-    )
-    return vertices, edges
-
-def draw_cube(vertices, edges):
-    glBegin(GL_LINES)
-    for edge in edges:
-        for vertex in edge:
+def draw_model(vertices, faces):
+    glBegin(GL_TRIANGLES)
+    for face in faces:
+        for vertex in face:
             glVertex3fv(vertices[vertex])
     glEnd()
 
-def lidar_scan(center, num_rays=360, max_distance=10):  # Lidar scan sim from the center
+def lidar_scan(center, vertices, faces, num_rays=360, max_distance=10): # Lidar scan sim
     angle_step = 360 / num_rays
     for angle in np.arange(0, 360, angle_step):
         rad = math.radians(angle)
@@ -33,11 +32,15 @@ def lidar_scan(center, num_rays=360, max_distance=10):  # Lidar scan sim from th
             x = center[0] + distance * math.cos(rad)
             y = center[1] + distance * math.sin(rad)
             z = center[2]
-            # Intersections check here
-            # (we will just draw the ray because its laggy already)
-            glBegin(GL_POINTS)
-            glVertex3f(x, y, z)
-            glEnd()
+            for face in faces:
+                v0, v1, v2 = vertices[face[0]], vertices[face[1]], vertices[face[2]]
+                # Implement ray-triangle intersection test heres
+                # If the ray intersects with the triangle defined by v0, v1, v2, visualize/record the intersection point
+                # Priklad: check_ray_triangle_intersection(ray_origin, ray_direction, v0, v1, v2)
+                # If intersection occurs, visualize or record the intersection point
+                glBegin(GL_POINTS)
+                glVertex3f(x, y, z)  # Visualize the LiDAR scan point
+                glEnd()
 
 def main():
     pygame.init()
@@ -46,7 +49,9 @@ def main():
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
     glTranslatef(0, 0, -20)
 
-    vertices, edges = create_cube()
+    vertices, faces = load_obj(r"C:\Users\horak\Documents\GitHub\Raycasting\raycasting-3D-map\map.obj")
+
+    start_position = (5, 0, 0) # of the LIDAR
 
     while True:
         for event in pygame.event.get():
@@ -56,8 +61,11 @@ def main():
 
         glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        draw_cube(vertices, edges)
-        lidar_scan((0, 0, 0), 360, 10)
+        draw_model(vertices, faces)
+
+        # Starting pos
+        lidar_scan(start_position, vertices, faces, 360, 10)
+
         pygame.display.flip()
         pygame.time.wait(10)
 
