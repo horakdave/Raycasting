4da9ce295e02cc82ce6d1d5edccacfad57fa8a4d
horakdave <horakdejv@gmail.com>
2024-06-20 13:04:24 +0200

    IS POINTLESS ANYWAYS

--- raycasting-3D-map/test.py
+++ raycasting-3D-map/test.py
@@ -23,6 +23,14 @@ player_speed = 0.175
 
 # Player vision angle
 vision_angle = 60  # in degrees
+num_rays = 30  # Number of rays for vision
+ray_length = 100  # Length of each ray
+
+# Initial player direction
+player_direction = 0
+
+# Turning speed
+turning_speed = 0.175  # Adjust turning speed here
 
 # Main loop
 while True:
@@ -33,23 +41,24 @@ while True:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        player_pos[0] -= player_speed
+        player_direction -= turning_speed  # Turn left
     if keys[pygame.K_RIGHT]:
-        player_pos[0] += player_speed
+        player_direction += turning_speed  # Turn right
+
     if keys[pygame.K_UP]:
-        player_pos[1] -= player_speed
-    if keys[pygame.K_DOWN]:
-        player_pos[1] += player_speed
+        player_pos[0] += player_speed * math.cos(math.radians(player_direction))
+        player_pos[1] += player_speed * math.sin(math.radians(player_direction))
 
     # Draw the map with entities
     screen.fill(WHITE)
     pygame.draw.rect(screen, BLACK, (player_pos[0], player_pos[1], 50, 50))  # Player
     pygame.draw.rect(screen, BLACK, (cube_pos[0], cube_pos[1], 50, 50))  # Cube
 
-    # Draw player's vision cone
-    vision_radius = 100
-    start_angle = math.radians(-vision_angle / 2)
-    end_angle = math.radians(vision_angle / 2)
-    pygame.draw.arc(screen, RED, (player_pos[0] - vision_radius, player_pos[1] - vision_radius, 2 * vision_radius, 2 * vision_radius), start_angle, end_angle, 3)
+    # Draw player's vision rays
+    for i in range(num_rays):
+        angle = math.radians(player_direction - vision_angle / 2 + (i / (num_rays - 1)) * vision_angle)
+        end_x = player_pos[0] + ray_length * math.cos(angle)
+        end_y = player_pos[1] + ray_length * math.sin(angle)
+        pygame.draw.line(screen, RED, player_pos, (end_x, end_y), 2)
 
     pygame.display.flip()
